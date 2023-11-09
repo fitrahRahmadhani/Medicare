7d63f8be078f6b28b0980c19bff244701dc16e43
fitrahRahmadhani <2131710092@student.polinema.ac.id>
2023-11-09 19:04:53 +0700

    feat: add footer

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleNavbar() {
+  const navbarTabs = document.querySelector('.navbar-tabs');
+  if (navbarTabs.style.display === 'block') {
+    navbarTabs.style.display = 'none';
+  } else {
+    navbarTabs.style.display = 'block';
+  }
+}
+
+
